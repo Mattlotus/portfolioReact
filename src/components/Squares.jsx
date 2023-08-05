c8996a1656a67d4c5305f169f09138b36e7c773c
Mattlotus <mattlotus91@gmail.com>
2023-08-05 19:30:13 -0400

    new games

--- src/components/Squares.jsx
+++ src/components/Squares.jsx
@@ -13,17 +13,18 @@ function Squares() {
     <div className='squareBar'>
         <Link to='/Contact' className='contactLink' onClick='' >
             <div>
-        <button className='square'>CONTACT </button>
+              <img src="https://image.api.playstation.com/vulcan/ap/rnd/202211/1408/l3bg7nmfPpPzam5jOVlaUFww.jpg" alt="" className='square' />
         </div>
         </Link>
         <Link to='/Resume' className='resumeLink'>
         <div>
-        <button className='square'>RESUME</button>
+          <img src="https://media.gamestop.com/i/gamestop/11206859-f4095b2b" alt="" className='square'/>
         </div>
         </Link>
         <Link to='/Exp' className='exp'>
         <div>
-        <button className='square'> EXP</button>
+          <img src="https://upload.wikimedia.org/wikipedia/en/5/5b/Mortal_Kombat_1_key_art.jpeg" alt="" className='square'/>
+      
         </div>
         </Link>
     </div>
