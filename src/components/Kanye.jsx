a80e4d50abd32b9a8db7f5d52bf30376d837e4a8
Mattlotus <mattlotus91@gmail.com>
2023-08-06 18:55:44 -0400

    kanye

--- src/components/Kanye.jsx
+++ src/components/Kanye.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useEffect,useState } from 'react'
+import axios from 'axios'
 
 function Kanye() {
+  const [kanyeQuote, setkanyeQuote]=useState('')
+  const [count, setCount] =useState(0)
+
+  const getData = async () => {
+
+    const response = await axios.get ('https://api.kanye.rest/')
+    
+
+                  console.log(response.data.quote)
+                  setkanyeQuote(response.data.quote)
+                  console.log(kanyeQuote)
+  }
+  useEffect(()=>{
+    getData()
+  }, [count])
   return (
     <div>
       
         <div>
           
-          <img src="https://postergrail.com/cdn/shop/products/A_31.jpg?v=1678797664&width=2048" alt="" className='square'/>
+          <img src="https://postergrail.com/cdn/shop/products/A_31.jpg?v=1678797664&width=2048" alt="" className='square' onClick={()=> getData()}/>
         </div>
       
 
