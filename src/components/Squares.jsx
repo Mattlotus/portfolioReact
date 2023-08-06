e3d6682db3b622711bd3a27629915ab862171636
Mattlotus <mattlotus91@gmail.com>
2023-08-06 12:38:06 -0400

    new order

--- src/components/Squares.jsx
+++ src/components/Squares.jsx
@@ -29,14 +29,21 @@ function Squares() {
         </Link>
 
 
+        {/* <Link to='/Multiverse' className='multi'>
+        <div>
+        <img src="https://www.lionbanner.com/wp-content/uploads/2023/02/PNG-LOW-QULITY-1-FOR-WEB-USE.png" alt="" className='square' />
+        </div>
+        </Link> */}
+
+          <Link to='/Spacebattle' className='space'>
+        <img src="https://cdn1.epicgames.com/b156c3365a5b4cb9a01a5e1108b4e3f4/offer/EGS_STARWARSBattlefrontIICelebrationEdition_DICE_S2-1200x1600-11d040719a8457bbf36cabbe89b200db.jpg" alt="" className='square' />
+        </Link>
+
+
         <Link to='/Multiverse' className='multi'>
         <div>
         <img src="https://www.lionbanner.com/wp-content/uploads/2023/02/PNG-LOW-QULITY-1-FOR-WEB-USE.png" alt="" className='square' />
         </div>
-        </Link>
-
-          <Link to='/Spacebattle' className='space'>
-        <img src="https://cdn1.epicgames.com/b156c3365a5b4cb9a01a5e1108b4e3f4/offer/EGS_STARWARSBattlefrontIICelebrationEdition_DICE_S2-1200x1600-11d040719a8457bbf36cabbe89b200db.jpg" alt="" className='square' />
         </Link>
     </div>
   )
