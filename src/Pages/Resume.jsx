eebf091077d7aa982fcb3a20255b504bb964b18c
Mattlotus <mattlotus91@gmail.com>
2023-08-06 13:24:59 -0400

    small adjustments

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -9,7 +9,7 @@ function Resume() {
             <Navbar></Navbar>
             <Squares></Squares>
             <div>
-              <p className='html'>Resume and Refrences available upon request</p>
+              <h2 className='html'>Resume & Refrences available upon request</h2>
             <img src="https://www.resumego.net/wp-content/uploads/references-available-upon-request-1.png" alt="" className='languages'/>
             </div>
         </div>
