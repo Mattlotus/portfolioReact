fdb089988e608ef24bb7dfa60a85fce2f858b3f3
Mattlotus <mattlotus91@gmail.com>
2023-08-06 17:46:39 -0400

    erase page

--- src/Pages/Practice.jsx
+++ src/Pages/Practice.jsx
@@ -3,32 +3,8 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Practice() {
-    let quoteUrl = " ";
-  const [quote, setQuote] = useState([]);
-  const getData = async () => {
-    const response = await axios.get('https://api.kanye.rest/');
-    const info = await response.data;
-    console.log(info);
-    setQuote(info);
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-
-
-  return (
-    <div>
-        {quote.map((q, i) => {
-let author = q.author.split(",");
-        return (
-          <div className="Fwrapper" key={i}>
-            <div className="qtxt">{q.text}</div>
-            <div className="author">{author[0]}</div>
-          </div>
-        );
-      })}
-    </div>
-  )
+    
+  
 }
 
 export default Practice
