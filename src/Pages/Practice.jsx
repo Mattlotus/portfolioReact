79bede1b3e88e67ebf49b8d69a17684496a9c8e3
Mattlotus <mattlotus91@gmail.com>
2023-08-06 17:51:30 -0400

    experiment

--- src/Pages/Practice.jsx
+++ src/Pages/Practice.jsx
@@ -3,6 +3,21 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Practice() {
+    const [kanyeSays, setKanyeSays] = useState("")
+    
+    useEffect(()=>{
+
+    }, []);
+    axios.get('https://api.kanye.rest/').then((res) => {
+
+    });
+
+    return(
+        <div>
+            <button>Ye Says</button>
+            <p>{kanyeSays}</p>
+        </div>
+    )
     
   
 }
