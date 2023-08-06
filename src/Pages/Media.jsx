234ca3b170e7a6a562e56e8a983bcc83b1927acf
Mattlotus <mattlotus91@gmail.com>
2023-08-06 13:41:40 -0400

    new page again

--- src/Pages/Media.jsx
+++ src/Pages/Media.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import axios from 'axios';
+import './App.css';
+import { useEffect,useState } from 'react';
 
 function Media() {
+    const [kanyeQuote, setkanyeQuote]=useState({})
+  const [count, setCount] =useState(0)
+
+  const getData = async () => {
+
+    const response = await axios.get ('https://api.kanye.rest/')
+                  .then(response => console.log(response.data))
+                  // .then (freeGames => setFreeGames())
+                  .catch(err => console.log(err))
+
+                  console.log(response)
+  }
   return (
-    <div>Media</div>
+    <div>
+        <div className="App">
+      <button onClick={()=>setCount(count +1)}>Moitivation</button>
+      <p>{kanyeQuote?.response}Quote:</p>
+      <p>{kanyeQuote?.constructor}</p>
+
+      
+    </div>
+    </div>
   )
 }
 
