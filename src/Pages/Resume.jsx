f9ac4e835566b30440ac82ca0704671bc46c82bc
Mattlotus <mattlotus91@gmail.com>
2023-08-06 13:16:08 -0400

    new class name

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -8,7 +8,7 @@ function Resume() {
         <div>
             <Navbar></Navbar>
             <Squares></Squares>
-            <img src="https://www.resumego.net/wp-content/uploads/references-available-upon-request-1.png" alt="" className='square'/>
+            <img src="https://www.resumego.net/wp-content/uploads/references-available-upon-request-1.png" alt="" className='languages'/>
             <div></div>
         </div>
     </div>
