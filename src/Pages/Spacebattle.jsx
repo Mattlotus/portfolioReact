07f3c1f80e3c14ae92b1499a16ef4909a61017ce
Mattlotus <mattlotus91@gmail.com>
2023-08-06 03:22:39 -0400

    new display

--- src/Pages/Spacebattle.jsx
+++ src/Pages/Spacebattle.jsx
@@ -9,8 +9,10 @@ function Spacebattle() {
         <Squares></Squares>
         <div className='spaceContainer'>
             <img src="https://cdn1.epicgames.com/b156c3365a5b4cb9a01a5e1108b4e3f4/offer/EGS_STARWARSBattlefrontIICelebrationEdition_DICE_S2-1200x1600-11d040719a8457bbf36cabbe89b200db.jpg" alt="" className='misk' />
+            <div>
             <h1 className='code'>Copy code to vs code to play!</h1>
             <p className='hub'>https://github.com/Mattlotus/spaceBattle</p>
+            </div>
         </div>
     </div>
   )
